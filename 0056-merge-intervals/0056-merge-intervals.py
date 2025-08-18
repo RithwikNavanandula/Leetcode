7dab83006bff810ca3944504d425ba5a7f08903e
Rithwik Navanandula <140204524+RithwikNavanandula@users.noreply.github.com>
2025-08-18 14:27:08 +0530

    Time: 10 ms (63.46%), Space: 16 MB (64.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -7,10 +7,8 @@ class Solution(object):
         intervals.sort(key = lambda a:a[0])
         res = [intervals[0]]
         for i in intervals[1:]:
-          for j in res:
-            if j[0] <= i[0] <= j[1]:
-              j[1] = max(i[1], j[1])
-              break
+          if res[-1][0] <= i[0] <= res[-1][1]:
+            res[-1][1] = max(i[1], res[-1][1])
           else:
             res.append(i)
         return res
