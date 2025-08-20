8d16fe21e36518d1def13b1bbea4665725028109
Rithwik Navanandula <140204524+RithwikNavanandula@users.noreply.github.com>
2025-08-20 15:07:23 +0530

    Time: 164 ms (5%), Space: 15.9 MB (8.64%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n = len(word1)
+        m = len(word2)
+        x = [0]*(n+1)
+        dp = []
+        for i in range(m+1):
+            dp.append(deepcopy(x))
+        for i in range(1, n+1):
+            dp[0][i] = i
+        for i in range(1, m+1):
+            dp[i][0] = i
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[j-1] == word2[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
+        return dp[m][n]
